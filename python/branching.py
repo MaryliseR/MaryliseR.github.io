a10fc66171dd7ba94118dd6acbd65e196f3bcc28
User <user@vagelos-ve735-0690.apnd.wlan.private.upenn.edu>
2018-06-05 16:52:18 -0400

    njf

---
+++ python/branching.py
@@ -0,0 +1,7 @@
+fruits = ["apples", "sand", "paper", "rock"]
+if fruits[0] == "apple":
+    print("Yum!)
+elif fruits[0] == "cardboard" or fruits[0] == "sand":
+    print("Yuck!")
+else:
+    print("Not bad.")
