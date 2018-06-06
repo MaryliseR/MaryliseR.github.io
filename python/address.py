2d1075f4ecc97a16097d2829cbf239cf8483c89d
User <user@vagelos-ve735-0690.apnd.wlan.private.upenn.edu>
2018-06-06 16:32:30 -0400

    hlhl

---
+++ python/address.py
@@ -0,0 +1,9 @@
+address = {"name": "Marylise Rilliard",
+"street address": "706 W North Loop blvd",
+"city": "Austin",
+"state": "TX",
+"zip code": "78751"}
+
+print(address["name"])
+print(address["street address"])
+print(address["city"] + "," + ["state"] + ["zip code"])
