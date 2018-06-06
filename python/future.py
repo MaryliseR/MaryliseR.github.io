ab8d738cbc6111ba22c446b383578b3a7c4c372c
User <user@vagelos-ve735-0690.apnd.wlan.private.upenn.edu>
2018-06-06 11:00:36 -0400

    new changes

---
+++ python/future.py
@@ -0,0 +1,7 @@
+def greeting(year):
+    if year < 1900:
+        print("You're so old!")
+    elif 1900 < year < 2020:
+        print("You were born in the 20th century!")
+    else:
+        print("Wow! You're from the future!")
